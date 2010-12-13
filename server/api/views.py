830ab2e829cd31199d4e884e93b7d0f64b0f00b6
Christopher League <league@contrapunctus.net>
2010-12-13 17:31:22 -0500

    user API GET/POST working

--- server/api/views.py
+++ server/api/views.py
@@ -1,6 +1,32 @@
 # Create your views here.
 
-from django.http import HttpResponse
+from server.api.models import User
+from django.http import *
+import simplejson as json
 
-def boo(request):
-    return HttpResponse("Hello again world")
+def jr(code, data):
+    return HttpResponse(json.dumps(data, ensure_ascii=False),
+                        status=code, content_type='text/plain')
+
+def ok(data):
+    return jr(200, data)
+
+def all_users(request):
+    return ok([u.name for u in User.objects.all()])
+
+def one_user(request, name):
+    return ((post_user if request.method == "POST" else get_user)
+            (request, name))
+
+def get_user(request, name):
+    try:
+        u = User.objects.get(name=name)
+        return ok(u.public_key)
+    except User.DoesNotExist:
+        return jr(404, False)
+
+def post_user(request, name):
+    pk = request.raw_post_data
+    u, created = (User.objects.get_or_create
+                  (name=name, defaults={'public_key': pk}))
+    return ok(True) if created else jr(403, 'User already exists')
--- server/urls.py
+++ server/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls.defaults import *
+from server.api.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^boo/', 'server.api.views.boo')
+    (r'^users/$', all_users),
+    (r'^users/(?P<name>.+)$', one_user)
+
     # Example:
     # (r'^server/', include('server.foo.urls')),
 
