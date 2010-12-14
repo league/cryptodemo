9766faa883ba236ddbcd98de8c162a2a03340a0b
Christopher League <league@contrapunctus.net>
2010-12-13 23:04:38 -0500

    functional abstraction of paging mechanism and others.

--- client/crypto-ui.js
+++ client/crypto-ui.js
@@ -2,14 +2,31 @@
 var userNameOk = false
 
 $(document).ready(function(){
-
     $("#poolRemaining").text(poolRemaining())
     $("#generateKey").attr("disabled", "true")
+    synchronizePrivateKeyInputs()
+    initializePaging()
+    $("#userName").keyup(validateUserName)
+    validateUserName() // in case it's populated on reload
 
-    /* Need two sychronized input boxes, so we can reveal private key.
-       The obscured one will be canonical (used in calculations) so we
-       must update it as the clear one changes. */
+    $(document).mousemove({kind: 'mousemove'}, gatherEntropy)
+    $(document).keyup({kind: 'keyup'}, gatherEntropy)
+    $(document).keydown({kind: 'keydown'}, gatherEntropy)
 
+    $("#generateKey").click(function() {
+        $("#pleaseWait").show()
+        randInit()
+        for(i = 0; i < 100; i++) {
+            $("#log").append(" "+rand(100))
+            if(i%10 == 9) $("#log").append("<br>")
+        }
+    })
+})
+
+/* Need two sychronized input boxes, so we can reveal private key.
+   The obscured one will be canonical (used in calculations) so we
+   must update it as the clear one changes. */
+function synchronizePrivateKeyInputs() {
     $("#privateKeyClear").change(function(){
         $("#privateKeyObscure").val($(this).val())
     })
@@ -27,60 +44,32 @@ $(document).ready(function(){
         $(".hideWhenObscure").hide()
         $(".hideWhenClear").show()
     })
+}
 
-    /* Paging mechanism */
+function initializePaging() {
+    $("#registerLink").click({page: "register"}, selectPage)
+    $("#loginLink").click({page: "login"}, selectPage)
+    $("#sendLink").click({page: "send"}, selectPage)
+    $("#readLink").click({page: "read"}, selectPage)
+}
 
-    $("#registerLink").click(function(){
-        $(".page").hide()
-        $("#registerPage").show("fast")
-        $("#navbar li").removeClass("selected")
-        $("#registerLink").addClass("selected")
-    })
+function selectPage(e) {
+    $(".page").hide()
+    $("#"+e.data.page+"Page").show("fast")
+    $("#navbar li").removeClass("selected")
+    $("#"+e.data.page+"Link").addClass("selected")
+}
 
-    $("#loginLink").click(function(){
-        $(".page").hide()
-        $("#loginPage").show("fast")
-        $("#navbar li").removeClass("selected")
-        $("#loginLink").addClass("selected")
-    })
-
-    $("#sendLink").click(function(){
-        $(".page").hide()
-        $("#sendPage").show("fast")
-        $("#navbar li").removeClass("selected")
-        $("#sendLink").addClass("selected")
-    })
-
-    $("#readLink").click(function(){
-        $(".page").hide()
-        $("#readPage").show("fast")
-        $("#navbar li").removeClass("selected")
-        $("#readLink").addClass("selected")
-    })
-
-    validateUserName()
-    $("#userName").keyup(validateUserName)
-
-    $(document).mousemove(function(e){
-        if(!poolFull()) {
-            eventEntropy(e)
-            $("#poolRemaining").text(poolRemaining())
-            if(poolFull()) {
-                maybeEnableGenerateKey()
-                $(document).unbind('mousemove')
-            }
+function gatherEntropy(e){
+    if(!poolFull()) {
+        eventEntropy(e)
+        $("#poolRemaining").text(poolRemaining())
+        if(poolFull()) {
+            maybeEnableGenerateKey()
+            $(document).unbind(e.data.kind)
         }
-    })
-
-    $("#generateKey").click(function() {
-        $("#pleaseWait").show()
-        randInit()
-        for(i = 0; i < 100; i++) {
-            $("#log").append(" "+rand(100))
-            if(i%10 == 9) $("#log").append("<br>")
-        }
-    })
-})
+    }
+}
 
 function maybeEnableGenerateKey() {
     $("#generateKey").attr('disabled', !poolFull() || !userNameOk)
